a5d9439ca08cfe523322948dc2d50430722c7174
Darkin <darkinb255@gmail.com>
2020-01-16 20:08:33 +0200

    add message

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,16 @@
+const newMessage = document.querySelector(".new-message");
+newMessage.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    chatRoom.AddMessage(newMessage.message.value.trim()).then(()=>{
+        newMessage.reset();
+    }).catch(e=>{
+        console.log(e);
+    });
+   
+})
+
+
+
 const chat = document.querySelector('.chat-messages');
 const chatUI = new UI(chat);
 const chatRoom = new ChatRoom('gaming','user');

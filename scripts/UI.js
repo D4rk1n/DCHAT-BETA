974adda8d106a21589907a3d20a0ae9b1d27f30b
Darkin <darkinb255@gmail.com>
2020-01-17 12:00:24 +0200

    time sent done

--- scripts/UI.js
+++ scripts/UI.js
@@ -4,10 +4,13 @@ class UI {
     }
     view(message)
     {
+        
+        let date =dateFns.distanceInWordsToNow(message.time.toDate() , {addSuffix : true});
         const li = `
         <li class = "list-group-item bg-light message-item "> 
-        <div class = "text-warning mID">${message.id} : </div>
+        <div class = "text-warning mID">${message.id}</div> 
         <div class = "mtxt">${message.messagetxt}</div>
+        <div class = "mdate"> ${date}</div>
         </li>
         `;
         this.chat.innerHTML+=li;
